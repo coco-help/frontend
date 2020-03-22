a7b815efe99991ddd9e33d8f4e2b4c6390f967d3
Moritz <Moritz.guehring@gmail.com>
2020-03-22 23:34:11 +0100

    education

--- src/views/Education.vue
+++ src/views/Education.vue
@@ -1,5 +1,43 @@
 <template lang="html">
-    <div>
-        test
+    <div class="education">
+        <div class="header">
+            <div class="back"></div>
+            <h1>Alles was du zu Covid-19 wissen solltest.</h1>
+        </div>
+        <div class="content">
+            <h2>Uhrsprung</h2>
+            <p>Anfang des Jahres 2020 hat eine neue Art von Virus aufgrund seiner unerwarteten Ausbreitungsgeschwindigkeit damit begonnen, die Schlagzeilen überall auf der Welt zu kontrollieren.
+
+Von seinem Ursprung auf einem Markt in Wuhan in China im Dezember 2019 bis hin zu Ländern, die so weit entfernt wie die Vereinigten Staaten von Amerika sind, hat der Virus, dessen offizieller Name COVID-19 lautet, tausende Menschen mit einer steigenden Todesrate Anfang des Jahres 2020 hat eine neue Art von Virus aufgrund seiner unerwarteten Ausbreitungsgeschwindigkeit damit begonnen, die Schlagzeilen überall auf der Welt zu kontrollieren.
+
+Von seinem Ursprung auf einem Markt in Wuhan in China im Dezember 2019 bis hin zu Ländern, die so weit entfernt wie die Vereinigten Staaten von Amerika sind, hat der Virus, dessen offizieller Name COVID-19 lautet, tausende Menschen mit einer steigenden Todesrate</p>
+            <a></a>
+        </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.education {
+  display: flex;
+  flex-flow: column;
+  .header {
+    background-color: #b06270;
+    width: 100vw;
+    height: 30vh;
+    padding: 40px 30px;
+    .back {
+    }
+    h1 {
+      font-weight: 600;
+      font-size: 26px;
+      line-height: 30px;
+      color: #ffffff;
+      width: 255px;
+    }
+  }
+  .content {
+    margin: 40px 30px;
+    max-width: 600px;
+  }
+}
+</style>
